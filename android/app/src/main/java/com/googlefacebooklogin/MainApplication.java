babf39df9ff6111f45024a8f2ccaf6bec2af6cba
samad324 <a.s324889@gmail.com>
2019-04-12 01:05:04 +0500

    facebook and google auth done android!

--- android/app/src/main/java/com/googlefacebooklogin/MainApplication.java
+++ android/app/src/main/java/com/googlefacebooklogin/MainApplication.java
@@ -4,17 +4,25 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+   private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+   protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -26,7 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNGoogleSigninPackage(),
-            new FBSDKPackage()
+             new FBSDKPackage(mCallbackManager)
       );
     }
 
